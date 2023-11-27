25b9c75614aa4a6477276425bca67bea9359d7d3
Titouan Launay <titouan.launay@lleedpartners.com>
2023-11-27 21:09:20 +0100

    Adds remote LLM processing POC

---
+++ packages/remote/llama.py
@@ -0,0 +1,81 @@
+import os
+
+from modal import Image, Secret, Stub, method
+
+MODEL_DIR = "/model"
+BASE_MODEL = "codellama/CodeLlama-7b-Instruct-hf"
+
+def download_model_to_folder():
+    from huggingface_hub import snapshot_download
+
+    os.makedirs(MODEL_DIR, exist_ok=True)
+
+    snapshot_download(
+        BASE_MODEL,
+        local_dir=MODEL_DIR,
+        token=os.environ["HUGGINGFACE_TOKEN"],
+    )
+
+image = (
+    Image.from_registry("nvcr.io/nvidia/pytorch:23.10-py3")
+    .pip_install(
+        "torch==2.1.0+cu121", index_url="https://download.pytorch.org/whl"
+    )
+    # Pinned to 10/16/23
+    .pip_install(
+        "vllm @ git+https://github.com/vllm-project/vllm.git@665cbcec4b963f6ab7b696f3d7e3393a7909003d"
+    )
+    # Use the barebones hf-transfer package for maximum download speeds. No progress bar, but expect 700MB/s.
+    .pip_install("hf-transfer~=0.1")
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
+    .run_function(
+        download_model_to_folder,
+        secret=Secret.from_name("huggingface"),
+        timeout=60 * 20,
+    )
+)
+
+stub = Stub("example-vllm-inference", image=image)
+
+@stub.cls(gpu="A100", secret=Secret.from_name("huggingface"))
+class Model:
+    def __enter__(self):
+        from vllm import LLM
+
+        # Load the model. Tip: MPT models may require `trust_remote_code=true`.
+        self.llm = LLM(MODEL_DIR)
+        self.template = """<s>[INST] <<SYS>>
+{system}
+<</SYS>>
+
+{user} [/INST] """
+
+    @method()
+    def generate(self, user_questions):
+        from vllm import SamplingParams
+
+        prompts = [
+            self.template.format(system="", user=q) for q in user_questions
+        ]
+
+        sampling_params = SamplingParams(
+            temperature=0.75,
+            top_p=1,
+            max_tokens=800,
+            presence_penalty=1.15,
+        )
+        result = self.llm.generate(prompts, sampling_params)
+        num_tokens = 0
+        for output in result:
+            num_tokens += len(output.outputs[0].token_ids)
+            print(output.prompt, output.outputs[0].text, "\n\n", sep="")
+        print(f"Generated {num_tokens} tokens")
+
+@stub.local_entrypoint()
+def main():
+    model = Model()
+    questions = [
+        # Coding questions
+        "Implement a Python function to compute the Fibonacci numbers.",
+    ]
+    model.generate.remote(questions)
