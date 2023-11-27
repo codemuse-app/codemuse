2d418e24243d392e4690fe6d4fa2141e6419c66e
Auguste Lefevre <augustelefevre@Augustes-MacBook-Pro.local>
2023-11-27 14:28:42 +0100

    add findMultiUpdateOrder

--- packages/vscode/src/extension/service/graph/utils_graph.ts
+++ packages/vscode/src/extension/service/graph/utils_graph.ts
@@ -58,19 +58,44 @@ export function findCycles(graph: MultiDirectedGraph): string[][] {
    a node is only processed after all its descendants (in this case, the nodes that depend on it) 
    have been processed. */
 
-   function findUpdateOrder(graph: MultiDirectedGraph, updatedNode: string): string[] {
-    let order: string[] = [];
-    let visited: Set<string> = new Set();
+export function findUpdateOrder(graph: MultiDirectedGraph, updatedNode: string): string[] {
+  let order: string[] = [];
+  let visited: Set<string> = new Set();
   
-    const visit = (node: string) => {
-      if (!visited.has(node)) {
-        visited.add(node);
-        graph.inNeighbors(node).forEach(visit); // Visit parent nodes
-        order.push(node); // Add the node after visiting parents (reverse topological sort)
-      }
-    };
+  const visit = (node: string) => {
+    if (!visited.has(node)) {
+      visited.add(node);
+      graph.inNeighbors(node).forEach(visit); // Visit parent nodes
+      order.push(node); // Add the node after visiting parents (reverse topological sort)
+    }
+  };
   
-    visit(updatedNode); // Start from the updated node
-    return order.reverse(); // Reverse to get the correct update order
-  }
+  visit(updatedNode); // Start from the updated node
+  return order.reverse(); // Reverse to get the correct update order
+}
+
+/* The findMultiUpdateOrder function takes a list of updated nodes and returns a list of nodes
+    in the order in which they should be updated. The function first performs a DFS on the graph 
+    starting from each updated node to identify all nodes that are affected by the update. It then 
+    performs a reverse topological sort on the affected nodes to determine the order in which they 
+    should be updated. */
+
+export function findMultiUpdateOrder(graph: MultiDirectedGraph, updatedNodes: string[]): string[] {
+  let depths: Map<string, number> = new Map();
+  let allNodes: Set<string> = new Set();
+
+  const visit = (node: string, depth: number) => {
+    if (!allNodes.has(node) || depths.get(node)! < depth) {
+      allNodes.add(node);
+      depths.set(node, depth);
+      graph.inNeighbors(node).forEach(parent => visit(parent, depth + 1));
+    }
+  };
+
+  updatedNodes.forEach(node => visit(node, 0));
+
+  return Array.from(allNodes)
+    .sort((a, b) => depths.get(b)! - depths.get(a)!) // Sort by depth in descending order
+    .filter((node, index, self) => self.indexOf(node) === index); // Remove duplicates
+}
   
