7614ca66bd9032edbfa8228e08b04385aba391fd
Titouan Launay <titouan.launay@lleedpartners.com>
2023-11-30 17:05:30 +0100

    Temporary fix for ts error

--- packages/vscode/src/extension/router/index.ts
+++ packages/vscode/src/extension/router/index.ts
@@ -3,7 +3,8 @@ import { Index } from "../service";
 
 export const router = vrpcRouter({
   helloWorld: procedure((name: string) => `Hello ${name}!`),
-  query: procedure((text: string) => Index.getInstance().query(text)),  
+  //@ts-expect-error
+  query: procedure((text: string) => Index.getInstance().query(text)),
 });
 
 export type RouterType = typeof router;
