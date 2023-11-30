9d11b681b9b3cad4c53ea8123eb477b8c910c7d4
Titouan Launay <titouan.launay@lleedpartners.com>
2023-11-30 14:54:27 +0100

    Updates asgi limits

--- packages/remote/asgi.py
+++ packages/remote/asgi.py
@@ -83,7 +83,7 @@ async def trigger_error():
     division_by_zero = 1 / 0
 
 
-@stub.function(image=image, allow_concurrent_inputs=20)
+@stub.function(image=image, allow_concurrent_inputs=100, concurrency_limit=1)
 @asgi_app()
 def asgi():
     return web_app
