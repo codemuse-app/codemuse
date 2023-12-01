f4b5ceccfadeff497fcb6b0b0f62cdea5a9af2f6
Titouan Launay <titouan.launay@lleedpartners.com>
2023-12-01 20:12:16 +0100

    Instruments activation and indexing

--- packages/vscode/src/extension/activate.ts
+++ packages/vscode/src/extension/activate.ts
@@ -23,60 +23,64 @@ export const activate = async (context: vscode.ExtensionContext) => {
   // Sentry set the installation ID
   Sentry.setUser({ id: getInstallationId(context) });
 
-  Index.initialize(context);
+  Sentry.startSpan({ name: "activate" }, async () => {
+    Index.initialize(context);
 
-  Status.getInstance();
+    Status.getInstance();
 
-  // Create a command called "CodeMuse: Index Workspace" that will run the index
-  context.subscriptions.push(
-    vscode.commands.registerCommand("codemuse.index", async () => {
-      Sentry.withScope(async (scope) => {
-        scope.setTransactionName("codemuse.index");
-        await Index.getInstance().run();
-      });
-    })
-  );
+    // Create a command called "CodeMuse: Index Workspace" that will run the index
+    context.subscriptions.push(
+      vscode.commands.registerCommand("codemuse.index", async () => {
+        Sentry.startSpan({ name: "index" }, async () => {
+          await Index.getInstance().run();
+        });
+      })
+    );
 
-  // Attach the search view
-  const searchViewProvider = new SearchViewProvider(context);
+    // Attach the search view
+    const searchViewProvider = new SearchViewProvider(context);
 
-  context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider(
-      "codemuse.sidebar",
-      searchViewProvider,
-      {
-        webviewOptions: {
-          // TODO: switch to true
-          retainContextWhenHidden: false,
-        },
-      }
-    )
-  );
+    context.subscriptions.push(
+      vscode.window.registerWebviewViewProvider(
+        "codemuse.sidebar",
+        searchViewProvider,
+        {
+          webviewOptions: {
+            // TODO: switch to true
+            retainContextWhenHidden: false,
+          },
+        }
+      )
+    );
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand("codemuse.openSidebar", () => {
-      searchViewProvider.show();
-    })
-  );
+    context.subscriptions.push(
+      vscode.commands.registerCommand("codemuse.openSidebar", () => {
+        searchViewProvider.show();
+      })
+    );
 
-  context.subscriptions.push(
-    // Command to trigger an error in Sentry
-    vscode.commands.registerCommand("codemuse.error", () => {
-      throw new Error("This is an error");
-    })
-  );
+    context.subscriptions.push(
+      // Command to trigger an error in Sentry
+      vscode.commands.registerCommand("codemuse.error", () => {
+        throw new Error("This is an error");
+      })
+    );
 
-  // Attach the CodeLens provider
-  let selector: vscode.DocumentSelector = {
-    scheme: "file",
-    language: "python",
-  };
-  context.subscriptions.push(
-    vscode.languages.registerCodeLensProvider(selector, new CodeMuseCodeLens())
-  );
+    // Attach the CodeLens provider
+    let selector: vscode.DocumentSelector = {
+      scheme: "file",
+      language: "python",
+    };
+    context.subscriptions.push(
+      vscode.languages.registerCodeLensProvider(
+        selector,
+        new CodeMuseCodeLens()
+      )
+    );
 
-  // Run the index command on startup
-  vscode.commands.executeCommand("codemuse.index");
+    // Run the index command on startup
+    vscode.commands.executeCommand("codemuse.index");
+  });
 };
 
 export const deactivate = () => {
