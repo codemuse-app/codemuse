daa4499f2354b715020e85623311f7125cdba0fd
Titouan Launay <titouan.launay@lleedpartners.com>
2023-12-01 16:15:14 +0100

    Adds sentry reporting

--- packages/vscode/src/app/index.tsx
+++ packages/vscode/src/app/index.tsx
@@ -1,7 +1,25 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 
 import { Search } from "./views/search";
+
+Sentry.init({
+  dsn: "https://6ec7abf2f59c9bb9cd7c8679a248cc8f@o4506308721115136.ingest.sentry.io/4506321118035968",
+  integrations: [new Sentry.Replay()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 try {
   const root = document.getElementById("root-search");
