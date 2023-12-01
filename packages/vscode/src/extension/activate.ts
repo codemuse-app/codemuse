e2a19a03ae3dcc31445fef43f8cf20840b18f981
Titouan Launay <titouan.launay@lleedpartners.com>
2023-12-01 16:21:03 +0100

    Disables cpu profiling for traces

--- packages/vscode/src/extension/activate.ts
+++ packages/vscode/src/extension/activate.ts
@@ -7,12 +7,11 @@ import { getInstallationId } from "./track";
 import { CodeMuseCodeLens } from "./codelense";
 
 import * as Sentry from "@sentry/node";
-import { ProfilingIntegration } from "@sentry/profiling-node";
 
 export const activate = async (context: vscode.ExtensionContext) => {
   Sentry.init({
     dsn: "https://6ec7abf2f59c9bb9cd7c8679a248cc8f@o4506308721115136.ingest.sentry.io/4506321118035968",
-    integrations: [new ProfilingIntegration()],
+    integrations: [],
     tracesSampleRate: 1.0,
     profilesSampleRate: 1.0,
   });
