39ae6924c6dcc61b601a04f57848a77c8a8fd27a
Auguste Lefevre <augustelefevre@Augustes-MacBook-Pro.local>
2023-12-01 15:10:37 +0100

    codelense fixed

--- packages/vscode/src/extension/codelense.ts
+++ packages/vscode/src/extension/codelense.ts
@@ -1,6 +1,8 @@
 import * as vscode from "vscode";
 import { Index } from "./service";
 import { LocalGraphNode } from "./service/graph/types";
+import { get } from "http";
+import { getSymbolName } from "../shared/utils";
 
 // Create a new VSCode CodeLens provider
 export class CodeMuseCodeLens implements vscode.CodeLensProvider {
@@ -20,14 +22,22 @@ export class CodeMuseCodeLens implements vscode.CodeLensProvider {
         let records = [];
         for (const node of graph!.nodes()) {
             const nodeData = graph!.getNodeAttributes(node);
-            if (nodeData) {
-                records.push(nodeData as LocalGraphNode);
+
+            if (!(nodeData as LocalGraphNode).file) {
+                continue;
+            }
+
+            const data = nodeData as LocalGraphNode;
+
+            if (data.file === document.fileName && data.range) {
+                records.push(data);
             }
         }
 
         // Each record has a field called called file: string and range: [number, number, number, number];
         // For each of these records, we need to create a new CodeLens object
         const codeLenses = records.map((record) => {
+            
             const range = new vscode.Range(
                 new vscode.Position(record.range[0], record.range[1]),
                 new vscode.Position(record.range[2], record.range[3])
@@ -35,18 +45,19 @@ export class CodeMuseCodeLens implements vscode.CodeLensProvider {
 
             const lenses = [];
 
-            const codeLens = new vscode.CodeLens(range, {
-                title: "🧠 CodeMuse",
-                command: "extension.askCodeMuse",
-                arguments: [record, range],
-                tooltip: "Ask CodeMuse to explain " + record.symbol,
-            });
+            // const codeLens = new vscode.CodeLens(range, {
+            //     title: "🧠 CodeMuse",
+            //     command: "extension.askCodeMuse",
+            //     arguments: [record, range],
+            //     tooltip: "Ask CodeMuse to explain " + record.symbol,
+            // });
 
-            lenses.push(codeLens);
+            // lenses.push(codeLens);
 
             if (record.content && record.symbol) {
+                const record_info = getSymbolName(record.symbol);
                 const codeLens2 = new vscode.CodeLens(range, {
-                    title: "Documentation",
+                    title: "📄 " + record_info.name,
                     command: "extension.askCodeMuseDoc",
                     arguments: [record, range],
                     tooltip: "Ask an explanation for " + record.symbol,
