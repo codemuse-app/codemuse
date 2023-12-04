7a9e630704334213085daa114bc445a002f92531
Titouan Launay <titouan.launay@lleedpartners.com>
2023-12-04 18:20:06 +0100

    Adds progress indicator

--- packages/vscode/src/extension/router/index.ts
+++ packages/vscode/src/extension/router/index.ts
@@ -7,10 +7,10 @@ import { capture } from "../service/logging/posthog";
 
 export const router = vrpcRouter({
   helloWorld: procedure((name: string) => `Hello ${name}!`),
-  query: procedure((text: string) => {
+  query: procedure(async (text: string) => {
     capture("query");
 
-    return Sentry.startSpan(
+    return await Sentry.startSpan(
       {
         op: "function",
         name: "query",
