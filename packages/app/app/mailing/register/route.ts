dbfe469be05594828904051bff3b28d0bcef745f
Titouan Launay <titouan.launay@lleedpartners.com>
2023-12-07 16:27:11 +0100

    Adds mailing list handlers

---
+++ packages/app/app/mailing/register/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import * as mailchimp from "@mailchimp/mailchimp_marketing";
+
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_SERVER_PREFIX,
+});
+
+export async function POST(request: Request) {
+  // Receives a post request with a form data containing both email and redirect fields.
+  const data = await request.formData();
+
+  try {
+    mailchimp.lists.addListMember(process.env.MAILCHIMP_LIST_ID as string, {
+      email_address: data.get("email") as string,
+      status: "subscribed",
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  if (data.get("redirect")) {
+    return NextResponse.redirect(data.get("redirect") as string);
+  }
+
+  return NextResponse.redirect("/");
+}
