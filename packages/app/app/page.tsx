731dc06fe5da4c39c1218b8b623b7977469ca796
Titouan Launay <titouan.launay@lleedpartners.com>
2023-12-07 15:03:56 +0100

    Triggers a new app build

--- packages/app/app/page.tsx
+++ packages/app/app/page.tsx
@@ -1,26 +1,26 @@
-import DeployButton from '../components/DeployButton'
-import AuthButton from '../components/AuthButton'
-import { createClient } from '@/utils/supabase/server'
-import ConnectSupabaseSteps from '@/components/ConnectSupabaseSteps'
-import SignUpUserSteps from '@/components/SignUpUserSteps'
-import Header from '@/components/Header'
-import { cookies } from 'next/headers'
+import DeployButton from "../components/DeployButton";
+import AuthButton from "../components/AuthButton";
+import { createClient } from "@/utils/supabase/server";
+import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
+import SignUpUserSteps from "@/components/SignUpUserSteps";
+import Header from "@/components/Header";
+import { cookies } from "next/headers";
 
 export default async function Index() {
-  const cookieStore = cookies()
+  const cookieStore = cookies();
 
   const canInitSupabaseClient = () => {
     // This function is just for the interactive tutorial.
     // Feel free to remove it once you have Supabase connected.
     try {
-      createClient(cookieStore)
-      return true
+      createClient(cookieStore);
+      return true;
     } catch (e) {
-      return false
+      return false;
     }
-  }
+  };
 
-  const isSupabaseConnected = canInitSupabaseClient()
+  const isSupabaseConnected = canInitSupabaseClient();
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -30,7 +30,7 @@ export default async function Index() {
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
-
+      CodeMuse
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
@@ -38,10 +38,9 @@ export default async function Index() {
           {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
         </main>
       </div>
-
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
-          Powered by{' '}
+          Powered by{" "}
           <a
             href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
             target="_blank"
@@ -53,5 +52,5 @@ export default async function Index() {
         </p>
       </footer>
     </div>
-  )
+  );
 }
