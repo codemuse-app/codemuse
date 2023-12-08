05bc2323ab8c957ed71913bdb731fc4836e971ac
Titouan Launay <titouan.launay@lleedpartners.com>
2023-12-08 11:01:24 +0100

    Fixes token with Supabase API response

--- packages/remote/asgi.py
+++ packages/remote/asgi.py
@@ -61,7 +61,7 @@ class Api:
 
             try:
                 # Check if the token is valid in supabase
-                db_token = self.supabase.table("api_tokens").select("*").eq("id", token).single().execute()
+                db_token = self.supabase.table("api_tokens").select("*").eq("id", token).single().execute().data
             except Exception as e:
                 raise HTTPException(500, e.__str__)
 
