4cf9bac7376e9442bb65008a51bec1a6eccaf426
Titouan Launay <titouan.launay@lleedpartners.com>
2023-12-08 12:17:17 +0100

    Refactors the route handler

--- packages/app/app/auth/user/route.ts
+++ packages/app/app/auth/user/route.ts
@@ -1,6 +1,5 @@
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
 
 // Exposes a route that takes a refresh token and returns a new access token
 // This is used by the VSCode extension to refresh the access token
@@ -8,19 +7,29 @@ import { cookies } from "next/headers";
 export async function POST(request: Request) {
   const { token } = await request.json();
 
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PRIVATE_SUPABASE_KEY!
+  );
 
   const { data, error } = await supabase
     .from("api_tokens")
     .select("*")
-    .eq("id", token);
+    .eq("id", token)
+    .single();
 
   if (error) {
-    return new Response(error.message, { status: 401 });
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      {
+        status: 401,
+      }
+    );
   }
 
   return NextResponse.json({
-    userId: data[0].user_id,
+    userId: data.user_id,
   });
 }
