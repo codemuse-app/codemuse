196044e46d90c7b057df06b1180618dc22de176d
Titouan Launay <titouan.launay@lleedpartners.com>
2023-12-08 15:37:06 +0100

    Adds logging to the github button

--- packages/app/components/auth/githubButton.tsx
+++ packages/app/components/auth/githubButton.tsx
@@ -10,9 +10,9 @@ export const GitHubLoginButton = ({ redirectTo }: { redirectTo?: string }) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: `${
-          window.location.origin
-        }/auth/callback?redirectTo=${encodeURIComponent(redirectTo || "/")}`,
+        redirectTo: `/auth/callback?redirectTo=${encodeURIComponent(
+          redirectTo || "/"
+        )}`,
       },
     });
 
@@ -43,6 +43,7 @@ export const GitHubLoginButton = ({ redirectTo }: { redirectTo?: string }) => {
         />
       </svg>
       Sign in with Github
+      {redirectTo}
     </button>
   );
 };
