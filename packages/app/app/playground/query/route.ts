e34ca2373b30953e80020c9148bd4ec2f3fe396f
Titouan Launay <titouan.launay@lleedpartners.com>
2023-12-26 20:20:15 +0100

    Adds marketing website dev server

--- packages/app/app/playground/query/route.ts
+++ packages/app/app/playground/query/route.ts
@@ -92,7 +92,7 @@ export async function GET(request: Request) {
       headers: {
         // CORS allow from codemuse.app and localhost
         "Access-Control-Allow-Origin":
-          "https://codemuse.app, http://localhost:3000",
+          "https://codemuse.app, http://localhost:3000, http://localhost:4321",
       },
     }
   );
